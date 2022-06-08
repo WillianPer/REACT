146fc8e8c25ea40795288c85bbae2beea3776a41
WilianPer <wsresilienttasteshopw@gmail.com>
2022-06-07 23:25:55 -0400

    Componentes de classe usando React!!

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -2,19 +2,23 @@ import React /*,{ Fragment, Component} */from 'react'
 
 import ReactDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
 // import {BoaTarde, BoaNoite} from './componentes/Multiplos' 
 
 // import BoaTarde, {BoaNoite} from './componentes/Multiplos'
 
-import Mult, {BoaNoite, AteMais} from './componentes/Multiplos'  /* BoaTarde foi importado fora da {},
+/* import Mult, {BoaNoite, AteMais} from './componentes/Multiplos'  /* BoaTarde foi importado fora da {},
 porque foi exportado usando Default*/
 
+import Saudacao from './componentes/Saudacao'
+
 ReactDOM.render(<div>
-    <BomDia nome="Willian" idade={10}/>
+    {/* <BomDia nome="Willian" idade={10}/> */}
     {/* <BoaTarde nome="Bia"></BoaTarde> */}
-    <Mult.BoaTarde nome="Bia"/>
-    <BoaNoite nome="Carla"></BoaNoite>
-    <AteMais nome="Milton"></AteMais>
+    {/* <Mult.BoaTarde nome="Bia"/> */}
+    {/* <BoaNoite nome="Carla"></BoaNoite> */}
+    {/* <AteMais nome="Milton"></AteMais> */}
+
+    <Saudacao tipo="Bom Dia" nome="Willian"></Saudacao>
 </div>
 , document.getElementById('root'))
