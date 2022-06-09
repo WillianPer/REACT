52aa41beac61581f80061c8762b96bae1bdc080c
WilianPer <wsresilienttasteshopw@gmail.com>
2022-06-09 14:12:18 -0400

    Componentes de classe usando React parte 2!!

--- exercicios/src/componentes/Saudacao.jsx
+++ exercicios/src/componentes/Saudacao.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-constructor */
 import React, {Component} from "react";
 
 export default class Saudacao extends Component {
@@ -5,6 +6,14 @@ export default class Saudacao extends Component {
     state = {
         tipo: this.props.tipo,
         nome: this.props.nome
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)
+        // this.setNome = this.setNome.bind(this)
+
     }
 
     setTipo(e) {
@@ -25,7 +34,9 @@ export default class Saudacao extends Component {
             <div>
                 <h1>{tipo} {nome}!!</h1>
                 <hr/>
-                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)}/>
+                {/* <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)}/> */}
+                {/* <input type="text" placeholder="Nome..." value={nome} onChange={e => this.setNome(e)}/> */}
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={this.setTipo}/>
                 <input type="text" placeholder="Nome..." value={nome} onChange={e => this.setNome(e)}/>
             </div>
         )
